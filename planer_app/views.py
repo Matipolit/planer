abed1170c6204c83fead37de30c303bc7c9cb9d4
Mateusz Polito <matipolit@gmail.com>
2023-11-14 14:19:27 +0100

    init new views

--- planer_app/views.py
+++ planer_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
-# Create your views here.
+def login(request):
+    return HttpResponse(request)
+
+
