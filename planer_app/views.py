02f8574f9ebf2eda5d50906fc042e9e0d51e874e
Mateusz Polito <matipolit@gmail.com>
2023-11-14 14:20:56 +0100

    login template

--- planer_app/views.py
+++ planer_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def login(request):
-    return HttpResponse(request)
+    return render(request, "login.html")
 
 
