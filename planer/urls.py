062e1f71931fd011e028b0d2119a058be74ecbfa
Mateusz Polito <matipolit@gmail.com>
2023-11-25 16:34:43 +0100

    add login, logout, basic interface

--- planer/urls.py
+++ planer/urls.py
@@ -1,28 +1,18 @@
-"""planer URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.contrib.auth import logout
 from django.urls import path, include, reverse_lazy
 from planer_app import views
 from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.shortcuts import redirect
 
-
+def logout_view(request):
+    logout(request)
+    return(redirect("/planer/login"))
 
 urlpatterns = [
-    path("planer/admin/", admin.site.urls),
-    #path("planer/", include("planer_app.urls")),
-    path('planer/login/', LoginView.as_view(template_name='login.html', next_page=reverse_lazy('/')), name='login'),
 
+    path("planer/", include("planer_app.urls")),
+    path("planer/admin/", admin.site.urls),
+    path("planer/login/", LoginView.as_view(template_name="login.html",next_page=("/planer/")), name='login'),
+    path("planer/logout/", logout_view, name="logout"),
 ]
