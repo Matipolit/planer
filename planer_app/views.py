564c8a1d8578d91c7c0fe68924143229aab7e827
Mateusz Polito <matipolit@gmail.com>
2023-11-28 09:55:01 +0100

    delete tasks

--- planer_app/views.py
+++ planer_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from .models import Task
+import json 
 
 
 @login_required(login_url='login')
@@ -20,13 +21,18 @@ def expenses(request: HttpRequest):
 
 
 @staff_member_required(login_url="login")
-def tasks_manage(request: HttpRequest):
+def tasks_manage(request):
     if(request.method == "POST"):
         vars = request.POST
-        instance = Task.objects.create(name = vars["name"], frequency = vars["frequency"])
-        print(f"Manage tasks instance: {instance}")
-
+        print(f"hello from post {vars}")
+        Task.objects.create(name = vars["name"], frequency = vars["frequency"])
+    elif(request.method == "DELETE"):
+        vars = json.loads(request.DELETE)
+        id = vars["id"]
+        instance = Task.objects.get(id=id)
+        print(f"Deleting instance {instance}")
+        instance.delete()
+        return JsonResponse({"deleted": "true"})
     tasks = Task.objects.all()
-    print(f"tasks: {tasks}")
     context = {"user_name": request.user.first_name, "tasks": tasks}
     return render(request, "tasks_manage.html", context)
