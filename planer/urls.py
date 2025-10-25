523930b71d07b82c1d6996900aa0730fd620e3e2
Mateusz Polito <matipolit@gmail.com>
2025-10-25 13:13:57 +0200

    prepare for multi-branch deployment

--- planer/urls.py
+++ planer/urls.py
@@ -1,18 +1,32 @@
 from django.contrib import admin
 from django.contrib.auth import logout
 from django.urls import path, include, reverse_lazy
+from django.conf import settings
 from planer_app import views
-from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth.views import (
+    LoginView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.shortcuts import redirect
+
+URL_PREFIX = getattr(settings, "URL_PREFIX", "planer")
+
 
 def logout_view(request):
     logout(request)
-    return(redirect("/planer/login"))
+    return redirect("/planer/login")
+
 
 urlpatterns = [
-
-    path("planer/", include("planer_app.urls")),
-    path("planer/admin/", admin.site.urls),
-    path("planer/login/", LoginView.as_view(template_name="login.html",next_page=("/planer/")), name='login'),
-    path("planer/logout/", logout_view, name="logout"),
+    path(f"{URL_PREFIX}/", include("planer_app.urls")),
+    path(f"{URL_PREFIX}/admin/", admin.site.urls),
+    path(
+        f"{URL_PREFIX}/login/",
+        LoginView.as_view(template_name="login.html", next_page=f"/{URL_PREFIX}/"),
+        name="login",
+    ),
+    path(f"{URL_PREFIX}/logout/", logout_view, name="logout"),
 ]
