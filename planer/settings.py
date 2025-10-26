d0b992e9114505a09240d5dde9a6a6505772cda0
Mateusz Polito <matipolit@gmail.com>
2025-10-26 11:49:30 +0100

    allow media location to be configurable

--- planer/settings.py
+++ planer/settings.py
@@ -152,7 +152,7 @@ STATICFILES_DIRS = [
 
 # Media files (User uploads)
 MEDIA_URL = f"/{URL_PREFIX}/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT", os.path.join(BASE_DIR, "media"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
